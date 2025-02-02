8b8408a1ef5d5d4b4dfc8b9dd96c151fa7ee2c14
Joydip Bhattacharyya <joydipb01@gmail.com>
2025-02-02 15:48:41 +0530

    :tada: added code files and requirements

---
+++ src/agentic_graph.py
@@ -0,0 +1,22 @@
+from langgraph.graph import END, StateGraph, add_messages
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import BaseMessage, AIMessage, convert_to_messages
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from langchain_core.retrievers import BaseRetriever
+from langchain_huggingface import HuggingFaceEmbeddings, HuggingFacePipeline
+from langchain_core.output_parsers import BaseOutputParser
+
+class ClassifyQuestion(BaseModel):
+    """Binary score for hallucination present in generation answer."""
+
+    question_label: str = Field(
+        description="Question is of type 'general' or 'resource budget' or 'plan'"
+    )
+
+class QuestionClassificationParser(BaseOutputParser):
+    def parse(self, text: str) -> ClassifyQuestion:
+        if "general" in text.lower():
+            return ClassifyQuestion(question_label = "general")
