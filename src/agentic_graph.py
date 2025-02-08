c78095a3ef2b845e1f15f86f4b584828b7fc18a9
Joydip Bhattacharyya <joydipb01@gmail.com>
2025-02-08 12:14:39 +0530

    :zap: added question classifier and rephraser

--- src/agentic_graph.py
+++ src/agentic_graph.py
@@ -8,7 +8,7 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.documents import Document
 from langchain_core.messages import BaseMessage, AIMessage, convert_to_messages
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts.prompt import PromptTemplate
 from pydantic import BaseModel, Field
 from langchain_core.retrievers import BaseRetriever
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFacePipeline
@@ -18,7 +18,7 @@ class ClassifyQuestion(BaseModel):
     """Question label to classify question for further processing"""
 
     question_label: str = Field(
-        description="Question is of type 'general' or 'resource budget' or 'plan'"
+        description="Question is of type 'general' or 'resource budget' or 'comms' or 'misc'"
     )
 
 class QuestionClassificationParser(BaseOutputParser):
@@ -31,7 +31,10 @@ class QuestionClassificationParser(BaseOutputParser):
         elif "resource budget" in text.lower():
             return ClassifyQuestion(question_label = "resource budget")
         
-        return ClassifyQuestion(question_label = "plan")
+        elif "comms" in text.lower():
+            return ClassifyQuestion(question_label = "comms")
+        
+        return ClassifyQuestion(question_label="misc")
 
 class GraphState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
@@ -42,3 +45,31 @@ class GraphState(TypedDict):
 class GraphConfig(TypedDict):
     retriever: BaseRetriever
     llm: Pipeline
+
+def rephrase_question(state: GraphState, config: GraphConfig):
+    question = state["question"]
+    llm = config["configurable"]["llm"]
+
+    question_rephraser_prompt = PromptTemplate.from_file('prompts/prompt_question_rephraser.txt')
+
+    question_rephraser = question_rephraser_prompt | llm | StrOutputParser()
+    better_question = question_rephraser.invoke({"question": question})
+
+    return {"question": better_question}
+
+def classify_question(state: GraphState, config: GraphConfig):
+    question = state["question"]
+    llm = config["configurable"]["llm"]
+
+    question_classifier_prompt = PromptTemplate.from_file('prompts/prompt_question_classifier.txt')
+
+    question_classifier = question_classifier_prompt | llm | QuestionClassificationParser()
+    question_label: ClassifyQuestion = question_classifier.invoke(
+        {"question": question}
+    )
+
+workflow = StateGraph(GraphState, GraphConfig)
+
+workflow.add_node()
+
+graph = workflow.compile()
